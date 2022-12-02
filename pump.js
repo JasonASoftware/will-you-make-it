35bdb16d1542992f1e82d4dca17e8b80f2f961fa
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-12-01 19:46:57 -0500

    create a js file

---
+++ pump.js
@@ -0,0 +1,4 @@
+// just multiplying fuel left and mpg and has to be greater or equal to the distance
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+return fuelLeft * mpg >= distanceToPump
+}
